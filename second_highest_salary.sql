daca8bbd49ffe035251104913a9d67b3a5a9916a
Nicolle <58828437+nicollelynnho@users.noreply.github.com>
2022-03-02 22:55:57 -0600

    Update second_highest_salary.sql

--- second_highest_salary.sql
+++ second_highest_salary.sql
@@ -1,11 +1,22 @@
 /*Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.*/
 
-select t2.nextsal as SecondHighestSalary
-from 
-(select *,LAG(salary,1) over (order by salary)  as nextsal
-from
-(Select *
-from 
-Employee
-order by salary desc ) t1 ) t2
-limit 1
+SELECT
+   t2.nextsal as SecondHighestSalary 
+FROM
+   (
+      SELECT *,
+         LAG(salary, 1) over (
+      ORDER BY
+         salary) as nextsal 
+      FROM
+         (
+            SELECT
+               * 
+            FROM
+               Employee 
+            ORDER BY
+               salary DESC 
+         )
+         t1 
+   )
+   t2 limit 1
